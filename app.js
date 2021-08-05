493fc1825777d638e03c9c3ac998795a0e98d4e6
Raj Thombare (Rakaa) <86057670+Raj-Thombare@users.noreply.github.com>
2021-08-05 15:48:55 +0530

    style done

--- app.js
+++ app.js
@@ -1,37 +1,63 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const btnCheck = document.querySelector("#btn-check");
+const btnContinue = document.querySelector("#btn-continue");
+const errorMsg = document.querySelector("#error-message");
+const cashGivenDiv = document.querySelector(".cash-given-div")
+const output = document.querySelector("#output-div")
 const numberOfNotes = document.querySelectorAll(".number-of-notes");
 
 const notes = [2000, 500, 100, 20, 10, 5, 1];
 
-btnCheck.addEventListener("click", getBillAmountAndCashGiven)
 
-function getBillAmountAndCashGiven() {
-    if(billAmount.value){
-        if (billAmount.value > 0) {
-            if(cashGiven.value>0){
-                if (cashGiven.value >= billAmount.value) {
-                    let cashToBeReturned = cashGiven.value - billAmount.value;
-                    calculateNotes(cashToBeReturned);
-                } else {
-                    alert("Do you wanna wash some plates!!??")
-                }
-            }else{
-                alert("please enter the cash to be given!")
-            }
+
+function checkBtnHandler() {
+    let bill = Number(billAmount.value);
+    let cash = Number(cashGiven.value);
+    hideError();
+    hideTable();
+    if (cash > 0) {
+        if (cash >= bill) {
+            let cashToBeReturned = cash - bill;
+            calculateNotes(cashToBeReturned);
         } else {
-            alert("invalid Bill Amount!")
+            errorMessage("Do you wanna wash some Bartans🍽️🍴🥄")
         }
-    }else{
-        alert("please enter Bill Amount")
+    } else {
+        errorMessage("Please enter Cash Amount to be paid!💵")
     }
-}
+};
 
-function calculateNotes(cashToBeReturned){
-    for(let i=0; i<notes.length; i++){ 
+function calculateNotes(cashToBeReturned) {
+    for (let i = 0; i < notes.length; i++) {
         let noOfNotes = Math.trunc(cashToBeReturned / notes[i]);
         cashToBeReturned = cashToBeReturned % notes[i];
         numberOfNotes[i].innerText = noOfNotes;
     }
+    output.style.display = "block";
 }
+
+function continueBtnHandler() {
+    hideError();
+    if (Number(billAmount.value > 0)) {
+        cashGivenDiv.style.display = "block";
+        btnContinue.style.display = "none"
+    } else {
+        errorMessage("Invalid Bill Amount!💵");
+    }
+}
+
+function errorMessage(text){
+    errorMsg.style.display = "block";
+    errorMsg.innerText = text;
+}
+
+function hideError(){
+    errorMsg.style.display = "none";
+}
+function hideTable() {
+    output.style.display = "none";
+}
+
+btnContinue.addEventListener("click", continueBtnHandler)
+btnCheck.addEventListener("click", checkBtnHandler)
